e59b3846c906b290303c6f564d5d6a03eb596a57
VictoriaDaneva <viktoriadanevas2003s@gmail.com>
2024-09-15 15:59:31 +0300

    Add edit and delete
    only i cant fix is the image to be visible :(

--- handlers/cat.js
+++ handlers/cat.js
@@ -218,6 +218,187 @@ module.exports = (req, res) => {
       res.writeHead(302, { location: "/" });
       res.end();
     });
+  } else if (pathname.includes("/cats-edit") && req.method === "GET") {
+    let catId = pathname.split("/cats-edit/")[1];
+    let filepath = path.normalize(
+      path.join(__dirname, "../views/editCat.html")
+    );
+
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(404, {
+          "Content-Type": "text/plain",
+        });
+
+        res.write("404 not found :(");
+        res.end();
+        return;
+      }
+
+      const cat = cats.find((cat) => cat.id == catId);
+
+      fs.readFile("./data/breeds.json", "utf-8", (err, breedsData) => {
+        if (err) {
+          console.log(err);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.write("Internal server error");
+          res.end();
+          return;
+        }
+
+        const breeds = JSON.parse(breedsData);
+        const breedOptions = breeds
+          .map((breed) =>
+            breed === cat.breed
+              ? `<option value="${breed}" selected>${breed}</option>`
+              : `<option value="${breed}">${breed}</option>`
+          )
+          .join("\n");
+
+        let finalHtml = data.toString().replace("{{id}}", catId);
+        finalHtml = finalHtml.replace("{{name}}", cat.name);
+        finalHtml = finalHtml.replace("{{description}}", cat.description);
+        finalHtml = finalHtml.replace("{{catBreeds}}", breedOptions);
+        finalHtml = finalHtml.replace(
+          "{{image}}",
+          `/content/images/${cat.image}`
+        );
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(finalHtml);
+        res.end();
+      });
+    });
+  } else if (pathname.includes("/cat-find-new-home") && req.method === "GET") {
+    let catId = pathname.split("/cat-find-new-home/")[1];
+    let filepath = path.normalize(
+      path.join(__dirname, "../views/catShelter.html")
+    );
+
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(404, {
+          "Content-Type": "text/plain",
+        });
+
+        res.write("404 not found :(");
+        res.end();
+        return;
+      }
+
+      const cat = cats.find((cat) => cat.id == catId);
+
+      fs.readFile("./data/breeds.json", "utf-8", (err, breedsData) => {
+        if (err) {
+          console.log(err);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.write("Internal server error");
+          res.end();
+          return;
+        }
+
+        const breeds = JSON.parse(breedsData);
+        const breedOptions = breeds
+          .map((breed) =>
+            breed === cat.breed
+              ? `<option value="${breed}" selected>${breed}</option>`
+              : `<option value="${breed}">${breed}</option>`
+          )
+          .join("\n");
+
+        let finalHtml = data.toString().replace("{{id}}", catId);
+        finalHtml = finalHtml.replace("{{name}}", cat.name);
+        finalHtml = finalHtml.replace("{{description}}", cat.description);
+        finalHtml = finalHtml.replace("{{catBreeds}}", breedOptions);
+        finalHtml = finalHtml.replace(
+          "{{image}}",
+          `/content/images/${cat.image}`
+        );
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(finalHtml);
+        res.end();
+      });
+    });
+  } else if (pathname.includes("/cats-edit") && req.method === "POST") {
+    const catId = pathname.split("/cats-edit/")[1];
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const formData = qs.parse(body);
+      let catIndex = cats.findIndex((cat) => cat.id == catId);
+      catIndex = Number(catIndex);
+
+      cats[catIndex].name = formData.name;
+      cats[catIndex].description = formData.description;
+      cats[catIndex].breed = formData.breed;
+
+      const json = JSON.stringify(cats);
+      fs.writeFile(
+        path.join(__dirname, "../data/cats.json"),
+        json,
+        "utf-8",
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.write("Failed to save the updated cat data");
+            res.end();
+            return;
+          }
+
+          res.writeHead(302, { location: "/" });
+          res.end();
+        }
+      );
+    });
+  } else if (pathname.includes("/cat-find-new-home") && req.method === "POST") {
+    const catId = pathname.split("/cat-find-new-home/")[1];
+
+    const catIndex = cats.findIndex((cat) => cat.id == catId);
+
+    const removedCat = cats.splice(catIndex, 1)[0];
+
+    if (removedCat && removedCat.image) {
+      const imagePath = path.join(
+        __dirname,
+        "../content/images",
+        removedCat.image
+      );
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error("Failed to delete image file:", err);
+        } else {
+          console.log(`Image ${removedCat.image} deleted successfully.`);
+        }
+      });
+    }
+
+    const json = JSON.stringify(cats);
+
+    fs.writeFile(
+      path.join(__dirname, "../data/cats.json"),
+      json,
+      "utf-8",
+      (err) => {
+        if (err) {
+          console.error("Failed to save updated cats.json:", err);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.write("Failed to delete the cat");
+          res.end();
+          return;
+        }
+
+        res.writeHead(302, { location: "/" });
+        res.end();
+      }
+    );
   } else {
     true;
   }
